83cba42310c8f8787aa13e80ed9a68d07c3c010c
Eyes0n <eye3570@naver.com>
2020-12-08 01:23:05 +0900

    add webpack loader

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  modules: {
+    rules: [
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          // Add exports of a module as style to DOM
+          { loader: "style-loader" },
+
+          // Loads CSS file with resolved imports and returns CSS code
+          { loader: "css-loader" },
+
+          // Loads and compiles a SASS/SCSS file
+          { loader: "sass-loader" },
+        ],
+      },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   loader: "file-loader",
+      //   options: {
+      //     name: "[path][name].[ext]",
+      //   },
+      // },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+          name: "[path][name].[ext]",
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    mini - css - extract - plugin || extract - css - chunks - webpack - plugin,
+  ],
+};
