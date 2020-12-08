d61b7b10aad9ff8a276eff1e05a870d708b90990
Eyes0n <eye3570@naver.com>
2020-12-08 20:55:04 +0900

    add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV,
   entry: {
     main: './src/index.js',
   },
@@ -9,13 +15,18 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  modules: {
+  module: {
     rules: [
       {
         test: /\.((c|sa|sc)ss)$/i,
         use: [
           // Add exports of a module as style to DOM
-          { loader: 'style-loader' },
+          {
+            loader:
+              process.env.NODE_ENV === 'production'
+                ? MiniCssExtractPlugin.loader
+                : 'style-loader',
+          },
 
           // Loads CSS file with resolved imports and returns CSS code
           { loader: 'css-loader' },
@@ -51,5 +62,29 @@ module.exports = {
       },
     ],
   },
-  plugins: [],
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : '',
+      },
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
+      hash: process.env.NODE_ENV === 'production',
+    }),
+    process.env.NODE_ENV === 'production' &&
+      new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new CleanWebpackPlugin(),
+  ].filter(Boolean),
 };
